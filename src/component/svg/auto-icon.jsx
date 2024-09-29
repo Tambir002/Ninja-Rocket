701348e3e38a1b4c1bab457fe08b7b1fe4778bfb
Tambir002 <tambirmondal@gmail.com>
2024-09-29 22:57:24 +0530

    Add files via upload

---
+++ src/component/svg/auto-icon.jsx
@@ -0,0 +1,8 @@
+import AtuoIcon from "../../assets/icon/auto-icon.png"
+
+const AutoIcon = () => {
+  return (
+    <img src={AtuoIcon} width="20px" height="20px" className="max-w-5 h-5" alt="auto icon" />
+    )
+}
+export default AutoIcon;
