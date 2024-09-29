178509ed3b3dd2b2ccf06d2ed74b90084d41322b
Tambir002 <tambirmondal@gmail.com>
2024-09-29 22:43:41 +0530

    Add files via upload

---
+++ src/assets/avatar/index.jsx
@@ -0,0 +1,16 @@
+import avatarBeginner from "./Rank=Beginner.png"
+import avatarPilot from "./Rank=Pilot.png"
+import avatarExplorer from "./Rank=Explorer.png"
+import avatarAstronaut from "./Rank=Astronaut.png"
+import avatarCaptain from "./Rank=Captain.png"
+import avatarCommander from "./Rank=Commander.png"
+import avatarAdmiral from "./Rank=Admiral.png"
+import avatarLegend from "./Rank=Legend.png"
+import avatarMasterOfTheUniverse from "./Rank=Master of the Universe.png"
+import avatarGodOfSpace from "./Rank=God of Space.png"
+
+
+export const avatar = { avatarBeginner, avatarPilot, avatarExplorer,
+  avatarAstronaut, avatarCaptain, avatarCommander, avatarAdmiral,
+  avatarLegend, avatarMasterOfTheUniverse, avatarGodOfSpace
+ };
